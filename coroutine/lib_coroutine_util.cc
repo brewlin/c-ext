c3396b47094fc0adfa8fde05a55e024b08ea052c
xiaodo <97404667@qq.com>
2019-08-01 16:02:23 +0800

    push to argsinfo

--- coroutine/lib_coroutine_util.cc
+++ coroutine/lib_coroutine_util.cc
@@ -10,7 +10,20 @@ static PHP_METHOD(lib_coroutine_util,create);
 
 PHP_METHOD(lib_coroutine_util,create)
 {
-    php_printf("lib_coroutine_util,create\n");
+    //????
+    zend_fcall_info fci = empty_fcall_info;
+    zend_fcall_info_cache fcc = empty_fcall_info_cache;
+    zval result;
+    //1 -1 ????????? ??????
+    ZEND_PARSE_PARAMETERS_START(1,-1)
+        Z_PARAM_FUNC(fci,fcc)
+        Z_PARAM_VARIADIC("*",fci.params,fci.param_count)
+    ZEND_PARSE_PARAMETERS_END_EX(RETURN_FALSE);
+    fci.retval = &result;
+    if(zend_call_function(&fci,&fcc) != SUCCESS){
+        return;
+    }
+    *return_value = result;
 }
 
 const zend_function_entry lib_coroutine_util_methods[] = 
