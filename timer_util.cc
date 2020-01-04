9dd2c38d737fd20ebd60f480b1e8296b473cfc2f
brewlin <97404667@qq.com>
2020-01-04 22:41:50 +0800

    done timer tick

--- timer_util.cc
+++ timer_util.cc
@@ -1,34 +1,38 @@
 #include "php_lib.h"
+#include "lib_coroutine.h"
+#include "timer.h"
 
 zend_class_entry lib_timer_ce;
 zend_class_entry *lib_timer_ce_ptr;
 
-
 ZEND_BEGIN_ARG_INFO_EX(arginfo_lib_timer_arg, 0, 0, 2)
 ZEND_ARG_INFO(0, seconds)
-ZEND_ARG_CALLABLE_INFO(0,func,0)
+ZEND_ARG_CALLABLE_INFO(0, func, 0)
 ZEND_END_ARG_INFO()
 
+int tick(long long id,void *data){
 
+    php_lib_fci_fcc *fci = (php_lib_fci_fcc *)data;
+    zval result;
+    fci->fci.retval = &result;
+    if(zend_call_function(&fci->fci,&fci->fcc) != SUCCESS){
+        return NOMORE;
+    }
+    return NOMORE;
+}
 PHP_METHOD(timer_obj,tick)
 {
-    //????
-    zend_fcall_info fci = empty_fcall_info;
-    zend_fcall_info_cache fcc = empty_fcall_info_cache;
-    zval result;
+    php_lib_fci_fcc *fci = (php_lib_fci_fcc *)malloc(sizeof(php_lib_fci_fcc));
+
     zend_long seconds = 0;
-    //1 -1 ????????? ??????
-    ZEND_PARSE_PARAMETERS_START(1,2)
+    //强制传入两个参数
+    ZEND_PARSE_PARAMETERS_START(2, 2)
     Z_PARAM_LONG(seconds)
-    Z_PARAM_FUNC(fci,fcc)
-//    Z_PARAM_VARIADIC("*",fci.params,fci.param_count)
+    Z_PARAM_FUNC(fci->fci,fci->fcc)
     ZEND_PARSE_PARAMETERS_END_EX(RETURN_FALSE);
 
-    fci.retval = &result;
-    if(zend_call_function(&fci,&fcc) != SUCCESS){
-        return;
-    }
-    *return_value = result;
+    long id = create_time_event(seconds,tick,fci,NULL);
+    RETURN_LONG(id);
 }
 
 PHP_METHOD(timer_obj,after)
